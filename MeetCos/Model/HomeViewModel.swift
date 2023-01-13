292c8b3c87961a47864d8695d345ca29ffbd2d14
Nobuo1106 <gonnbe1986@gmail.com>
2023-01-13 20:20:02 +0900

    スタート時のタイマー追加

--- MeetCos/Model/HomeViewModel.swift
+++ MeetCos/Model/HomeViewModel.swift
@@ -6,19 +6,61 @@
 //
 
 import Foundation
+import Combine
+
 extension HomeView {
-    @MainActor class HomeViewModel: ObservableObject {
-        @Published var remainingTime: String = "0:00"
+    class HomeViewModel: ObservableObject {
+        @Published var interval: String = "0:00"
+        @Published var remainingTime: Double = 0.0
         @Published var stateDate = Date()
+        @Published var timer: AnyCancellable!
+        @Published var count: Int = 0
+        @Published var isTimer = true
         
         func calcRemainingTime (_ selected: Date)-> String{
             let interval = selected.timeIntervalSinceNow
+            self.remainingTime = interval
+            if self.remainingTime < 0 {
+                self.isTimer = false
+            }
+            print(self.remainingTime)
             let dateFormatter = DateComponentsFormatter()
             dateFormatter.unitsStyle = .positional
             dateFormatter.allowedUnits = [.hour, .minute]
             dateFormatter.zeroFormattingBehavior = .pad
             print(dateFormatter.string(from: interval)!)
             return dateFormatter.string(from: interval)!
+        }
+        
+        func start() {
+            self.count()
+        }
+        
+        func count(_ interval: Double = 0.1){
+            print("start Timer")
+            // TimerPublisherが存在しているときは念の為処理をキャンセル
+            if let _timer = timer{
+                _timer.cancel()
+            }
+
+            timer = Timer.publish(every: interval, on: .main, in: .common)
+                .autoconnect()
+                .receive(on: DispatchQueue.main)
+                .sink(receiveValue: ({_ in
+//                    self.count += 1
+                    self.remainingTime -= 0.1
+                    if self.remainingTime < 0 {
+                        self.stop()
+                    }
+            }))
+
+        }
+        
+        // タイマーの停止
+        func stop(){
+            print("stop Timer")
+            timer?.cancel()
+            timer = nil
         }
     }
 }
--- MeetCos/View/HomeView.swift
+++ MeetCos/View/HomeView.swift
@@ -11,24 +11,23 @@ struct HomeView: View {
     @StateObject private var viewModel = HomeViewModel()
     @State var selectddate = Date()
     @State var interval = "0:00"
+    @State var totalCost = 0
 
     var body: some View {
         VStack {
             HStack {
-                Text("終了日時")
+                Text("終了予定時刻")
                     .padding()
                 DatePicker("", selection: $selectddate, displayedComponents:
                         .hourAndMinute)
                 .onChange(of: selectddate, perform: { value in
                     interval = viewModel.calcRemainingTime(value)
-//                    print(type(of: value))
                    })
                 .environment(\.locale, Locale(identifier: "en_DK"))
                 .datePickerStyle(CompactDatePickerStyle())
                 .padding()
                 
             }
-            Text("残り時間   \(interval)")
             ZStack (alignment: .center){
                 Circle()
                     .stroke(Color.green, style: StrokeStyle(lineWidth:10))
@@ -39,10 +38,29 @@ struct HomeView: View {
                     .scaledToFit()
                     .padding(50)
                     .rotationEffect(.degrees(-90))
+                Text("残り時間   \(interval)")
+                    .onAppear {
+                        viewModel.start()
+                    }
+                
             }
-            
+            HStack {
+//                Text("\(viewModel.count)")
+//                Text("\(viewModel.remainingTime)")
+                Button("Start") {
+                    viewModel.start()
+                }
+                Button("Done") {
+                    viewModel.stop()
+                }
+            }
             Spacer()
-//            Text("Your current date is \(viewModel.stateDate)")
+            Text("\(totalCost)円")
+            Spacer()
+            Button ("Edit") {
+                
+            }
+            Spacer()
         }
         .padding()
     }
