0ca2781dd784e341016802ebf1e44395a33f1366
Nobuo1106 <gonnbe1986@gmail.com>
2023-04-11 07:26:48 +0900

    CountDownTimeViewModel作成

--- MeetCos/ViewModel/CountDownTimerViewModel.swift
+++ MeetCos/ViewModel/CountDownTimerViewModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import Combine
+
+class CountdownTimerViewModel: ObservableObject {
+    @Published var remainingTime: Double = 0.0
+    @Published var timer: AnyCancellable!
+    // ... other timer-related properties
+
+    // ... timer-related functions
+    func start() {
+        // ...
+    }
+
+    func stop() {
+        // ...
+    }
+
+    // ... other timer-related functions
+}
