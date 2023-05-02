e72544f67c1594b5d212dddfa5a9b93ed891edc2
Nobuo1106 <gonnbe1986@gmail.com>
2023-05-03 08:31:16 +0900

    ResultView追加

---
+++ MeetCos/View/ResultView.swift
@@ -0,0 +1,20 @@
+//
+//  ResultView.swift
+//  MeetCos
+//
+//  Created by apple on 2023/05/01.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultView()
+    }
+}
