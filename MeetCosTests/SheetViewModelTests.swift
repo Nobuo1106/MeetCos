6e17e6032fa4d166c614abc2c6ece195394b6f07
Nobuo1106 <gonnbe1986@gmail.com>
2023-05-28 11:20:06 +0900

    編集画面の総経費計算のテストを追加

---
+++ MeetCosTests/SheetViewModelTests.swift
@@ -0,0 +1,34 @@
+//
+//  MeetCosTests.swift
+//  MeetCosTests
+//
+//  Created by apple on 2023/05/27.
+//
+
+import XCTest
+@testable import MeetCos
+
+class SheetViewModelTests: XCTestCase {
+
+    var timePickerViewModel: TimePickerViewModel!
+    var sheetViewModel: SheetViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        timePickerViewModel = TimePickerViewModel()
+        sheetViewModel = SheetViewModel(timePickerViewModel: timePickerViewModel)
+    }
+    
+    func testCalculateSession() {
+        timePickerViewModel.hourSelection = 2
+        timePickerViewModel.minSelection = 30
+        sheetViewModel.expenses = [
+            Expense(personCount: 1, hourlyWage: 2000, hourlyProfit: 1000),
+            Expense(personCount: 2, hourlyWage: 3000, hourlyProfit: 5000)
+        ]
+        
+        let totalCost = sheetViewModel.calculateSession()
+        
+        XCTAssertEqual(totalCost, 47500)
+    }
+}
